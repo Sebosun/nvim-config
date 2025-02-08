bd9b77202087b80b2b7ddb8022b24b81be5fd82b
Sebastian <sebos@protonmail.com>
2025-02-08 14:08:43 +0100

    fix: alphavim icon fixes

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -13,16 +13,17 @@ dashboard.section.header.val = {
 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("F", "  Find hidden file", ":Telescope find_files hidden=true<CR>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("F", "󰘓  Find hidden file", ":Telescope find_files hidden=true<CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("o", "  Obsidian in neovim", ":e ~/Documents/obsidian-git-new <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("o", "  Obsidian in neovim", ":e ~/Documents/obsidian-git-new <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "   Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", "󰩈  Quit Neovim", ":qa<CR>"),
 }
+
 
 local function footer()
 	-- NOTE: requires the fortune-mod package to work
@@ -30,7 +31,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "gitlab.com/Sebosun"
+	return "github.com/Sebosun"
 end
 
 dashboard.section.footer.val = footer()
